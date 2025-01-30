9dfd098ced3b19be34367b3356cc198aac806a0f
Rahman-S <suonrahman91100@outlook.fr>
2025-01-30 11:51:37 +0100

    :hammer: Load API URL from configuration file

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 import requests
+import configparser
 
 def check_api_status(url): 
     try:
@@ -12,6 +13,16 @@ def check_api_status(url):
     except requests.exceptions.RequestException as e:
         return f"error: {e}"
 
-api_url = "https://jsonplaceholder.typicode.com/posts"
+def load_config():
+    try:
+        config = configparser.ConfigParser() # create a new instance of ConfigParser to read the config file
+        config.read("config.ini")
+        return config["DEFAULT"]["url"]
+    except (configparser.Error, KeyError) as e:
+        print(f"Error reading config file: {e}")
+        exit(1)
+
+api_url = load_config()
 status = check_api_status(api_url)
 print(f"Api status: {status}")
+
